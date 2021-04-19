1dc47056ddfb18b59d45bd4fa59ad34e7fa8573d
Andrew Hansen <andrewthansen@hotmail.com>
2021-04-19 15:02:41 -0500

    Add project files.

---
+++ HansenLambdaUnitTestExample/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HansenLambdaUnitTestExample
+{
+    public enum Condition
+    {
+        BAD,
+        FAIR,
+        GOOD,
+        EXCELLENT
+    }
+    public class Car
+    {
+        public static int _speed =0;
+        public string Make;
+        public Condition Condition;
+
+        public Car(string m, Condition c)
+        {
+            this.Make = m;
+            this.Condition = c;
+        }
+
+        public int Speed
+        {
+            get => _speed;
+            set
+            {
+                if (value == -51)
+                {
+                    _speed = -50;
+                }
+                else if(value == 201){
+                    _speed = 200;
+                }
+                else
+                {
+                    _speed = value;
+                }
+            }
+        }
+    }   
+}
+
